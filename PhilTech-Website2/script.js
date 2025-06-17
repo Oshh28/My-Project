bc284d526cdf48f144ae436ffb329dc722638ac2
Oshh28 <chrizyruze@gmail.com>
2025-06-17 08:28:30 +0800

    Update script.js

--- PhilTech-Website2/script.js
+++ PhilTech-Website2/script.js
@@ -1,18 +1,21 @@
   const popOutButton = document.getElementById("SHS-Button");
   const popOutButton2 = document.getElementById("COLLEGE-Button");
   const popOutButton3 = document.getElementById("SUNDAY-Button");
+  const popOutButton4 = document.getElementById("DEVELOPER-Button");
   const bg_PopOffers = document.querySelector(".Bg-PopOffers");
   const bg_PopOffers2 = document.querySelector(".Bg-PopOffers2");
   const bg_PopOffers3 = document.querySelector(".Bg-PopOffers3");
+  const bg_PopDev = document.querySelector(".BgDev-Popup");
   const cancelPop = document.getElementById("Close");
   const cancelPop2 = document.getElementById("Close2");
   const cancelPop3 = document.getElementById("Close3");
+  const cancelPop4 = document.getElementById("Close4");
 
   // Show popup on button click
   popOutButton.addEventListener("click", function (e) {
     e.preventDefault();
     bg_PopOffers.classList.add("show");
-    document.body.style.overflow = "hidden"; // prevent background scroll
+    document.body.style.overflow = "hidden";
   });
 
   popOutButton2.addEventListener("click", function (e) {
@@ -27,11 +30,17 @@
     document.body.style.overflow = "hidden";
   });
 
+  popOutButton4.addEventListener("click", function (e) {
+    e.preventDefault();
+    bg_PopDev.classList.add("show");
+    document.body.style.overflow = "hidden";
+  });
+
   // Close popup
   cancelPop.addEventListener("click", function (e) {
     e.preventDefault();
     bg_PopOffers.classList.remove("show");
-    document.body.style.overflow = "auto"; // re-enable scroll
+    document.body.style.overflow = "auto"; 
   });
 
   cancelPop2.addEventListener("click", function (e) {
@@ -45,6 +54,14 @@
     bg_PopOffers3.classList.remove("show");
     document.body.style.overflow = "auto";
   });
+
+  cancelPop4.addEventListener("click", function (e) {
+    e.preventDefault();
+    bg_PopDev.classList.remove("show");
+    document.body.style.overflow = "auto";
+  });
+
+  
 
 let toastBox = document.getElementById('toastBox');
 let successMsg = '<i class="fa-solid fa-circle-check"></i> Successfully submitted';
