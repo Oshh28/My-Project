aced8dc5995cb48af3c7352932059b1ba54b94fc
Oshh28 <chrizyruze@gmail.com>
2025-06-20 09:25:25 +0800

    Update script.js

--- Christian-PhilTech-Web/script.js
+++ Christian-PhilTech-Web/script.js
@@ -1,3 +1,20 @@
+const popOutButton4 = document.getElementById("DEVELOPER-Button");
+const bg_PopDev = document.querySelector(".BgDev-Popup");
+const cancelPop4 = document.getElementById("Close4");
+
+popOutButton4.addEventListener("click", function (e) {
+    e.preventDefault();
+    bg_PopDev.classList.add("show");
+    document.body.style.overflow = "hidden";
+  });
+
+cancelPop4.addEventListener("click", function (e) {
+    e.preventDefault();
+    bg_PopDev.classList.remove("show");
+    document.body.style.overflow = "auto";
+  });
+
+
 const swiper = new Swiper('.slider-wrapper', {
   loop: true,
   grabCursor: true,
@@ -28,3 +45,4 @@ const swiper = new Swiper('.slider-wrapper', {
     }
   }
 });
+
