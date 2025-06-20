828ff5352a984c76767a0ca8d8d8156bb2f2bb50
Oshh28 <chrizyruze@gmail.com>
2025-06-20 09:17:01 +0800

    Update aboutScript.js

--- PhilTech-Website2/aboutScript.js
+++ PhilTech-Website2/aboutScript.js
@@ -1,3 +1,15 @@
+ let lastScrollTop = 0;
+ navbar = document.getElementById("navbar");
+ window.addEventListener("scroll", function(){
+    let scrollTop = window.pageXOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop){
+      navbar.classList.add("active");
+    } else {
+      navbar.classList.remove("active");
+    }
+    lastScrollTop = scrollTop;
+ })
+ 
 const popOutButton4 = document.getElementById("DEVELOPER-Button");
 const bg_PopDev = document.querySelector(".BgDev-Popup");
 const cancelPop4 = document.getElementById("Close4");
